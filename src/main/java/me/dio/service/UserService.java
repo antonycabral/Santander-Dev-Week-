d0c4e322400648c138cde899e9c2fffe68c7c534
Antony Guilherme <83239872+antonycabral@users.noreply.github.com>
2024-08-20 20:47:39 -0300

    hotfix userservice

--- src/main/java/me/dio/service/UserService.java
+++ src/main/java/me/dio/service/UserService.java
@@ -1,9 +1,25 @@
 package me.dio.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import me.dio.model.User;
+import me.dio.repository.UserRepository;
 
-public interface UserService {
-    User FindById(Long id);
+@Service
+public class UserService {
 
-    User create(User userToCreate);
+    @Autowired
+    private UserRepository repository;
+
+    private User create(User user){
+        if (user.getId() != null && repository.existsById(user.getId())) {
+            throw new IllegalArgumentException("o user id ja existe");
+        }
+        return repository.save(user);
+    }
+    
+    public User findById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
 }
